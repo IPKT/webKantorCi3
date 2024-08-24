25cbfe68aab8c4f6b401d839b5407632ae964506
IPKT <putu.kembar@gmail.com>
2024-08-24 09:15:00 +0800

    dea

---
+++ application/views/artikel/artikel_20240823_1.php
@@ -0,0 +1,44 @@
+<div class="container">
+    <h3 class="text-center">SEISMISITAS WILAYAH BALI DAN SEKITARNYA PERIODE 16 - 22 AGUSTUS 2024
+    </h3>
+    <div class="container d-flex justify-content-center">
+        <img width="80%" src="<?php echo  base_url('gambar/artikel/artikel_20240823_1_1.png') ?>" alt="">
+    </div>
+    <br>
+
+    <p>Berdasarkan data Stasiun Geofisika Denpasar selama minggu ke-3 bulan Agustus 2024, di daerah Bali dan sekitarnya
+        telah terjadi 32 kejadian gempabumi dengan magnitudo bervariasi mulai dari M 1.6 sampai M 5. Kejadian gempabumi
+        dengan magnitudo M < 3 sejumlah 21 kejadian atau 66% dari total kejadian gempabumi. Sedangkan untuk M 3 – 5
+            sejumlah 10 kejadian atau 31% dari total kejadian gempabumi dan terdapat 1 kejadian gempabumi dengan
+            magnitudo M>5 atau 3% dari total kejadian gempabumi.
+
+    </p>
+    <div class="container d-flex justify-content-center">
+        <img width="80%" src="<?php echo  base_url('gambar/artikel/artikel_20240823_1_2.png') ?>" alt="">
+    </div>
+    <p>Berdasarkan kedalaman, gempabumi yang mendominasi adalah kejadian gempabumi dangkal yaitu kurang dari 60 km.
+        Jumlah gempabumi dangkal sebanyak 25 kejadian gempabumi atau 78% dari total kejadian gempabumi, terdapat
+        sebanyak 7 kejadian gempabumi dengan kedalaman menengah 60 km – 300 km atau 22% dari total kejadian gempabumi
+        dan tidak terdapat kejadian gempabumi dengan kedalaman dalam >300.
+    </p>
+
+    <div class="container d-flex justify-content-center">
+        <img width="80%" src="<?php echo  base_url('gambar/artikel/artikel_20240823_1_3.png') ?>" alt="">
+    </div>
+    <p>Pada periode ini, terdapat 1 gempabumi dirasakan. Gempabumi berkekuatan M 5.0 pada tanggal 21 Agustus 2024 pukul
+        18:58:30 WIB dengan kedalaman 48 km di 175 km Tenggara LOMBOKTENGAH-NTB.
+
+    </p>
+
+
+    <div class="container d-flex justify-content-center">
+        <img width="80%" src="<?php echo  base_url('gambar/artikel/artikel_20240823_1_4.png') ?>" alt="">
+    </div>
+    <p> Gempabumi kedua berkekuatan M. 4.9 pada tanggal 15 Agustus 2024 pukul 21:14:03 WIB dengan kedalaman 52 km di 141
+        km BaratDaya JEMBRANA-BALI.
+
+    </p>
+
+    <p></p>
+    <p>Salam sehat dan semangat. <br>Jumat, 23 Agustus 2024 2024</p>
+</div>
--- application/views/layout/press_release.php
+++ application/views/layout/press_release.php
@@ -18,31 +18,31 @@
         </div>
         <div class="card mb-2">
             <div class="card-body">
-                <h5 class="card-title">Gempabumi Wilayah Bali dan Sekitarnya periode periode 09 - 15 Agustus 2024
+                <h5 class="card-title">Gempabumi Wilayah Bali dan Sekitarnya periode periode 16 - 22 Agustus 2024
                 </h5>
-                <p class="card-text">Berdasarkan data Stasiun Geofisika Denpasar selama minggu ke-2 bulan Agustus 2024,
-                    di daerah Bali dan sekitarnya telah terjadi 28 kejadian gempabumi dengan magnitudo bervariasi mulai
-                    dari M 1.4 sampai M 4.9. ....</p>
-                <a href="<?php echo base_url('artikel/20240816_1') ?>" class="btn btn-primary btn-sm">Selengkapnya</a>
+                <p class="card-text">Berdasarkan data Stasiun Geofisika Denpasar selama minggu ke-3 bulan Agustus 2024,
+                    di daerah Bali dan sekitarnya telah terjadi 32 kejadian gempabumi dengan magnitudo bervariasi mulai
+                    dari M 1.6 sampai M 5. ....</p>
+                <a href="<?php echo base_url('artikel/20240823_1') ?>" class="btn btn-primary btn-sm">Selengkapnya</a>
             </div>
         </div>
         <div class="card mb-2">
             <div class="card-body">
-                <h5 class="card-title">Terbit dan Terbenam Bali dan Sekitarnya Periode 19 - 25 Agustus 2024
+                <h5 class="card-title">Terbit dan Terbenam Bali dan Sekitarnya Periode 26 Agustus - 1 September 2024
                 </h5>
-                <p class="card-text">Berdasarkan data Almanak BMKG selama 19 - 25 Agustus 2024, waktu terbit dan
-                    terbenam matahari di kota dan kabupaten di Bali bervariasi per harinya. Variasi waktu terbit dan
-                    terbenam matahari dipengaruhi oleh letak suatu tempat....... </p>
-                <a href="<?php echo base_url('artikel/20240816_2') ?>" class="btn btn-primary btn-sm">Selengkapnya</a>
+                <p class="card-text">Berdasarkan data Almanak BMKG selama 26 Agustus - 1 September 2024, waktu terbit
+                    dan terbenam matahari di kota dan kabupaten di Bali bervariasi per harinya. Variasi waktu terbit dan
+                    terbenam matahari dipengaruhi oleh letak suatu tempat. ...... </p>
+                <a href="<?php echo base_url('artikel/20240823_2') ?>" class="btn btn-primary btn-sm">Selengkapnya</a>
             </div>
         </div>
         <div class="card mb-2">
             <div class="card-body">
-                <h5 class="card-title">Sambaran Petir di Wilayah Bali Periode 9 - 15 Agustus 2024
+                <h5 class="card-title">Sambaran Petir di Wilayah Bali Periode 16 - 22 Agustus 2024
                 </h5>
-                <p class="card-text">Berdasarkan peta kerapatan sambaran petir wilayah Bali periode 9 - 15 Agustus 2024,
-                    aktivitas petir terjadi di wilayah Bali dengan termasuk dalam kategori Rendah < 8. ..... </p>
-                        <a href="<?php echo base_url('artikel/20240816_3') ?>"
+                <p class="card-text">Berdasarkan peta kerapatan sambaran petir wilayah Bali periode 16 - 22 Agustus,
+                    aktivitas petir terjadi di wilayah Bali dengan termasuk dalam kategori Rendah < 8.. ..... </p>
+                        <a href="<?php echo base_url('artikel/20240823_3') ?>"
                             class="btn btn-primary btn-sm">Selengkapnya</a>
             </div>
         </div>
