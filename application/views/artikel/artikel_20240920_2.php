af5a72610dbd4b402528c6d113843d0e4ed09a1b
IPKT <putu.kembar@gmail.com>
2024-09-20 23:36:27 +0800

    dasda

--- application/views/artikel/artikel_20240920_2.php
+++ application/views/artikel/artikel_20240920_2.php
@@ -2,11 +2,17 @@
     <h3 class="text-center">Terbit dan Terbenam Bali dan Sekitarnya Periode 23 - 29 September 2024
     </h3>
     <div class="container justify-content-center">
-        <img width="100%" src="<?php echo  base_url('gambar/artikel/artikel_20240913_2_1.png') ?>" alt="">
+        <img width="100%" src="<?php echo  base_url('gambar/artikel/artikel_20240920_2_1.png') ?>" alt="">
     </div>
     <br>
 
     <p>
+        Berdasarkan data Almanak BMKG selama 23 - 29 September 2024, waktu terbit dan terbenam matahari di kota dan
+        kabupaten di Bali bervariasi per harinya. Variasi waktu terbit dan terbenam matahari dipengaruhi oleh letak
+        suatu tempat. Selama periode tersebut, waktu terbit matahari antara 06.11 WITA hingga 06.03 WITA dan waktu
+        terbenam matahari antara 18.17 WITA hingga 18.13 WITA. Berikut infografis terbit dan terbenam matahari di
+        seluruh kabupaten/kota di Bali.
+
     </p>
 
     <p>Salam sehat dan semangat. <br>Jumat, 20 September 2024 </p>
